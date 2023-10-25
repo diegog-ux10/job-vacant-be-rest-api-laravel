2dc5600d9683b0a04ee0cab7fe53575adcdfc73d
diegogranados143@gmail.com <diego@codeando.edu.pe>
2023-10-25 11:07:14 -0400

    vip

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,12 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
     public function login(Request $request)
     {
-        $data = $request->all();
-        return response()->json(['mssg' => $data], 200);
+        $credentials = $request->only('email', 'password');
+        if (Auth::attempt($credentials)) {
+            $user = Auth::user();
+            return response()->json(['user' => $user]);
+        }
+        return response()->json(['error' => 'Invalid credentials'], 401);
     }
 }
