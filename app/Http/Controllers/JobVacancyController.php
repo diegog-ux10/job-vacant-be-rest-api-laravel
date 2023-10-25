8110912e54b6b6dc319a6fae24db4c92994792bb
diegogranados143@gmail.com <diego@codeando.edu.pe>
2023-10-25 15:31:05 -0400

    feat: job vacancy model, controller, migration, CRUD

---
+++ app/Http/Controllers/JobVacancyController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\JobVacancy;
+use Illuminate\Http\Request;
+
+class JobVacancyController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+
+    public function index()
+    {
+        $jobVacancies = JobVacancy::with('user')->get();
+        return response()->json($jobVacancies);
+    }
+
+    public function show($id)
+    {
+        $jobVacancy = JobVacancy::findOrFail($id);
+        return response()->json($jobVacancy);
+    }
+
+    public function store(Request $request)
+    {
+        $jobVacancy = JobVacancy::create($request->all());
+        return response()->json($jobVacancy, 201);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $jobVacancy = JobVacancy::findOrFail($id);
+        $jobVacancy->update($request->all());
+        return response()->json($jobVacancy);
+    }
+
+    public function destroy($id)
+    {
+        $jobVacancy = JobVacancy::findOrFail($id);
+        $jobVacancy->delete();
+        return response()->json(null, 204);
+    }
+}
---
+++ app/Models/JobVacancy.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class JobVacancy extends Model
+{
+    protected $fillable = ['title', 'description', 'salary'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    use HasFactory;
+}
